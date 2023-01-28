5d01ff3da5d8cd27f4c189aa9bcff514b299e540
John Watkins <johnwes87@protonmail.com>
2023-01-28 14:28:07 -0600

    Added button functionality

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
+let color = "black";
+
 function populateBoard(size) {
     let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove())
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for( let i = 0; i < 256; i++){
-    let square = document.createElement("div")
-    square.style.backgroundColor = "white";
-    board.insertAdjacentElement("beforeend", square)
-    }
+    let amount = size * size;
+
+    for( let i = 0; i < amount; i++){
+        let square = document.createElement("div")
+        square.addEventListener('mouseover', colorSquare)
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square)
+        }
 }
 
 populateBoard(16);
 
 function changeSize(input){
-    populateBoard(input);
+    if(input >= 2 && input <= 100){
+        populateBoard(input);
+    } else {
+        alert('Too many squares');
+    }
 }
 
-/* Input 
-let input = document.querySelector('input');
-input.addEventListener('onchange', () => changeSize(input.value))
-let submit = document.querySelector('#submit');
-submit.addEventListener('click', ()=> populateBoard())
-*/
+function colorSquare() {
+    if (color === 'rainbow'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }else {
+        this.style.backgroundColor = color
+    }
+    ;
+}
+
+function changeColor(choice) {
+    
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white')
+}
